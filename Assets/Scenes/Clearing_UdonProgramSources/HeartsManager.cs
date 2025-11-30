517ee354d34c8fd8f4a9a1a3f69a21814f79d09c
Lavndr <laorichard123@gmail.com>
2025-11-29 21:11:56 -0800

    hearts + popuptext

--- Assets/Scenes/Clearing_UdonProgramSources/HeartsManager.cs
+++ Assets/Scenes/Clearing_UdonProgramSources/HeartsManager.cs
@@ -3,10 +3,14 @@ using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
+using UnityEngine.UI;
 
 public class HeartsManager : UdonSharpBehaviour
 {
     public int hearts = 3;
+    public Image heart1;
+    public Image heart2;
+    public Image heart3;
     public int getHearts()
     {
         return hearts;
@@ -15,5 +19,8 @@ public class HeartsManager : UdonSharpBehaviour
     public void setHearts(int newHearts)
     {
         hearts = newHearts;
+        heart1.enabled = hearts >= 1;
+        heart2.enabled = hearts >= 2;
+        heart3.enabled = hearts >= 3;
     }
 }
